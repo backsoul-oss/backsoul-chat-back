a57e361076f94bc3ddd1e39a7611ec88fa3615bb
DanielSarmientoDev <danielsarmientodeveloper@gmail.com>
2021-10-23 02:43:19 -0500

    feat: typing and name user messages

--- src/ws/ws.service.ts
+++ src/ws/ws.service.ts
@@ -1,34 +1,42 @@
 import {
- SubscribeMessage,
- WebSocketGateway,
- OnGatewayInit,
- WebSocketServer,
- OnGatewayConnection,
- OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class WsService implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class WsService
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('WsService');
 
- @WebSocketServer() server: Server;
- private logger: Logger = new Logger('WsService');
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, data: any): void {
+    this.server.emit('msgToClient', {id:Math.floor(Math.random() * 10000001), user_name: data.user_name, message: data.message });
+  }
 
- @SubscribeMessage('msgToServer')
- handleMessage(client: Socket, message: string): void {
-  this.server.emit('msgToClient',  {user: client.id,message});
- }
+  @SubscribeMessage('typingMessage')
+  handleTypingMessage(client: Socket, data: any) {
+    const messageLength = data.length
+    this.server.emit('sendTyping', { user_name: data.user_name, messageLength });
+  }
 
- afterInit(server: Server) {
-  this.logger.log('Init');
- }
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
 
- handleDisconnect(client: Socket) {
-  this.logger.log(`Client disconnected: ${client.id}`);
- }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 
- handleConnection(client: Socket, ...args: any[]) {
-  this.logger.log(`Client connected: ${client.id}`);
- }
+    handleConnection(client: Socket, ...args: any[]) {
+    this.server.emit('connected', client.id);
+    this.logger.log(`Client connected: ${client.id}`);
+  }
 }
